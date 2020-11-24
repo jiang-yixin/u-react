400a8a260ef1da9124ee396feb1ce5068589e62f
Yixin Jiang <jiangyixin0910@gmail.com>
2020-11-24 22:06:25 +0100

    Remove all files

--- pics/src/api/proxy160.js
+++
@@ -1,5 +0,0 @@
-import axiois from 'axios'
-
-export default axiois.create({
-    baseURL: 'http://hv04.ovh.ifeelsmart.net:8989'
-})
--- pics/src/api/unsplash.js
+++
@@ -1,8 +0,0 @@
-import axiois from 'axios'
-
-export default axiois.create({
-    baseURL: 'https://api.unsplash.com',
-    headers: {
-        'Authorization': 'Client-ID d8dd36ce5da61ca88d83047737e45b978a1ec6087614b67fbeaeba69b592a397'
-    }
-})
